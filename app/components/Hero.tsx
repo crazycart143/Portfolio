40c3502ab7ee81cb9b59f4d94a6e8bf7373c9b38
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-03-25 11:22:31 +0800

    added scroll down

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -14,6 +14,7 @@ export default function Hero() {
           variants={fadeIn("right", "tween", 0.5, 1)}
           initial="hidden"
           whileInView="show"
+          viewport={{ once: "false", amount: 0.25 }}
         >
           Hello there,
         </motion.p>
@@ -22,6 +23,7 @@ export default function Hero() {
           initial="hidden"
           whileInView="show"
           className="text-[60px]"
+          viewport={{ once: "false", amount: 0.25 }}
         >
           I'm Keanu John
         </motion.h1>
@@ -29,6 +31,7 @@ export default function Hero() {
           variants={fadeIn("right", "tween", 1.5, 1)}
           initial="hidden"
           whileInView="show"
+          viewport={{ once: "false", amount: 0.25 }}
         >
           a <span className="font-bold">front-end web developer</span>
         </motion.p>
@@ -36,6 +39,7 @@ export default function Hero() {
           variants={fadeIn("right", "tween", 2, 1)}
           initial="hidden"
           whileInView="show"
+          viewport={{ once: "false", amount: 0.25 }}
           className="cursor-pointer rounded-xl mt-[30px] py-3 px-4 font-semibold bg-[#F2F2F4] text-black"
         >
           View my projects
@@ -46,6 +50,11 @@ export default function Hero() {
           enablePan={false}
           enableZoom={false}
           enableRotate={false}
+          enableDamping={false}
+          // maxAzimuthAngle={Math.PI / 30}
+          // maxPolarAngle={Math.PI}
+          // minAzimuthAngle={-Math.PI / 4}
+          // minPolarAngle={0}
         />
         <ambientLight />
         <spotLight
@@ -63,10 +72,24 @@ export default function Hero() {
         {/* <directionalLight color="white" position={[0, 0, 5]} /> */}
         {/* <pointLight position={[10, 10, 10]} /> */}
       </Canvas>
-      <h1 className="absolute bottom-[80px] text-white right-[45%] font-bold text-xl">
+      <motion.h1
+        variants={fadeIn("down", "tween", 2, 1)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: "false", amount: 0.25 }}
+        className="absolute bottom-[80px] text-white right-[45%] font-bold text-xl"
+      >
         Scroll Down
-      </h1>
-      <div className="rounded-full w-12 h-32 border-2 border-white absolute -bottom-[60px] right-[47.5%]"></div>
+      </motion.h1>
+      <motion.div
+        className="rounded-full w-12 h-32 border-2 border-white absolute flex justify-center items-center -bottom-[60px] right-[47.5%]"
+        variants={fadeIn("right", "tween", 2, 1)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: "false", amount: 0.25 }}
+      >
+        <motion.div className="rounded-full w-[20px] h-[20px] bg-white"></motion.div>
+      </motion.div>
     </>
   );
 }
