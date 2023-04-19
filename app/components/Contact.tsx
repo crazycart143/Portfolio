c890c27558b9dca8149e6cba26da14133ab823ad
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-04-19 10:43:53 +0800

    fixed bug with apos

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <div className="flex flex-col md:flex-row gap-x-10 py-[50px] md:py-[180px] md:justify-start lg:py-[150px] px-[50px] md:px-[50px] justify-center items-center lg:justify-center">
       <div className="flex flex-col gap-y-6 text-center md:text-start w-full lg:w-[500px]">
-        <h1 className="font-bold text-[40px]">Let's work together.</h1>
+        <h1 className="font-bold text-[40px]">Let&apos;s work together.</h1>
         <p className="text-[18px] lg:text-[22px] xl:text-[20px]">
           Contact me if you think that I am qualified for your project or
           request a ready made projects if you are in a hurry.
