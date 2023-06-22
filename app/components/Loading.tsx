328af723c8472082fcf1e5f2e57a19c2bba150b4
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-06-22 18:34:02 +0800

    fixed loading

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -10,7 +10,7 @@ export default function Loading() {
       className="flex items-center justify-center w-full h-[100vh]"
     >
       <div className="border-4 border-[#F2F2F4] rounded-xl w-[100px] h-[100px] flex justify-center items-center ">
-        <h1 className="font-bold text-[#F2F2F4] tracking-tighter text-2xl ">
+        <h1 className="font-bold text-[#F2F2F4] tracking-tighter text-[50px] ">
           <TypingText title="Ke" textStyles="text-center" />
         </h1>
       </div>
--- utils/motion.tsx
+++ utils/motion.tsx
@@ -178,10 +178,7 @@ export const footerVariants = {
 };
 
 export const TypingText = ({ title, textStyles }) => (
-  <motion.p
-    variants={textContainer}
-    className={`font-normal text-[50px] text-secondary-white ${textStyles}`}
-  >
+  <motion.p variants={textContainer} className={` ${textStyles}`}>
     {Array.from(title).map((letter, index) => (
       <motion.span variants={textVariant2} key={index}>
         {letter === " " ? "\u00A0" : letter}
