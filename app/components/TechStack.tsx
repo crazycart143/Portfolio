1942a0b78917eb5015b0be47fd02c67ca35fcfab
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-06-26 20:16:05 +0800

    fixed gltf

--- app/components/TechStack.tsx
+++ app/components/TechStack.tsx
@@ -1,6 +1,6 @@
 import React, { useLayoutEffect, useRef } from "react";
 import { Canvas } from "react-three-fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, CameraControls } from "@react-three/drei";
 import { useGLTF } from "@react-three/drei";
 import { useState } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
@@ -10,42 +10,22 @@ function GLTFLoader(props) {
   const { url, scale: initialScale } = props;
   const gltf = useGLTF(url, true);
   const [rotation, setRotation] = useState(0);
-  const { viewport, gl } = useThree();
+  const { viewport } = useThree();
+  const width = viewport.width;
+  const height = viewport.height;
+
+  const scale = Math.min(width / 0.2, height) / 10;
+  const aspect = width / height / 1.8;
+
+  const lodRef = useRef();
+
+  gltf.scene.scale.set(scale * aspect, scale, scale);
+  gltf.scene.position.x = -viewport.width / 20;
+  gltf.scene.position.y = -viewport.height / 20;
 
   useFrame(() => {
     setRotation((rotation) => rotation + 0.01);
   });
-
-  const lodRef = useRef();
-
-  useLayoutEffect(() => {
-    const onResize = () => {
-      const width = viewport.width;
-      const height = viewport.height;
-      const aspect = width / height / 1.8;
-      const scale = Math.min(width / 0.2, height) / 10;
-
-      gltf.scene.scale.set(scale * aspect, scale, scale);
-      gltf.scene.position.x = -viewport.width / 30;
-      gltf.scene.position.y = -viewport.height / 20;
-
-      // Increase pixel density for better quality
-      const pixelRatio = window.devicePixelRatio;
-      gl.setPixelRatio(pixelRatio);
-
-      // Update LOD distances based on new scale
-      lodRef.current.children.forEach((child, i) => {
-        child.distance = 50 * (i + 1) * scale;
-      });
-    };
-
-    onResize();
-
-    window.addEventListener("resize", onResize);
-    return () => {
-      window.removeEventListener("resize", onResize);
-    };
-  }, [viewport.width, viewport.height, gltf.scene, gl, lodRef]);
 
   const LODLevels = [0.5, 1, 2, 4];
   const LODMeshes = LODLevels.map((level) => {
@@ -61,43 +41,33 @@ function GLTFLoader(props) {
     lodRef.current.addLevel(LODMeshes[i], LODLevels[i]);
   }
 
-  useFrame(({ camera }) => {
-    lodRef.current.update(camera);
-  });
-
-  lodRef.current.rotation.x = 10;
+  lodRef.current.rotation.x = 2.6;
   lodRef.current.rotation.y = rotation;
-  lodRef.current.rotation.z = 23;
-  lodRef.current.position.z = -30;
+  lodRef.current.rotation.z = 3.1;
+  lodRef.current.position.z = 0;
 
   return <primitive object={lodRef.current} dispose={null} />;
 }
 
 export default function TechStack() {
   return (
-    <div className="w-full h-[90vh]">
-      <Canvas>
+    <div className="w-full h-[83vh]">
+      <Canvas camera={{ fov: 75, position: [0, 0, -30] }}>
         <OrbitControls
-          enablePan={true}
           enableZoom={false}
           enableRotate={true}
+          enablePan={true}
           enableDamping={true}
-          position={(0, 0, 0)}
-          // // maxAzimuthAngle={Math.PI / 30}
-          maxPolarAngle={Math.PI}
-          // // minAzimuthAngle={-Math.PI / 4}
         />
-        <ambientLight intensity={0.5} />
+        <ambientLight intensity={0.1} />
         <spotLight
-          intensity={10}
-          angle={Math.PI / 10}
-          position={[10, 120, 40]}
-          shadow-mapSize-width={2048}
-          shadow-mapSize-height={2048}
+          intensity={0.5}
+          angle={Math.PI / 50}
+          position={[10, 120, -250]}
         />
-
+        <pointLight position={[20, 5, -100]} />
         {/* <pointLight position={[20, 0, 3]} intensity={3} /> */}
-        <GLTFLoader url="techstack-keyboard-2.gltf" />
+        <GLTFLoader url="techstack-keyboard.gltf" />
         {/* <ambientLight intensity={0.1} /> */}
         {/* <directionalLight color="white" position={[0, 0, 5]} /> */}
         {/* <pointLight position={[10, 10, 10]} /> */}
